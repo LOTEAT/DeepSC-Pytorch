aa18d6a386167ef929f8bd06e1b01039872bfaa8
LOTEAT <zenglezhu@gmail.com>
2023-06-01 14:56:17 +0800

    refractor semantic encoder

--- models/channel.py
+++ models/channel.py
@@ -55,13 +55,64 @@ class Channel(nn.Module):
         x = inputs
         y = x + torch.randn_like(x) * n_std
         return y
-    
-    def fading(self, inputs, K=1, n_std=0.1, detector='MMSE'):
+
+    def fading(inputs, K=1, n_std=0.1, detector='MMSE'):
         x = inputs
         bs, sent_len, d_model = x.shape
         mean = math.sqrt(K / (2 * (K + 1)))
         std = math.sqrt(1 / (2 * (K + 1)))
-        x = tf.reshape(x, (bs, -1, 2))
+        x = x.view(bs, -1, 2)
         x_real = x[:, :, 0]
         x_imag = x[:, :, 1]
-        x_complex = tf.complex(x_real, x_imag)
+        x_complex = torch.complex(x_real, x_imag)
+
+        # create the fading factor
+        h_real = torch.randn(1) * std + mean
+        h_imag = torch.randn(1) * std + mean
+        h_complex = torch.complex(h_real, h_imag)
+        # create the noise vector
+        n = torch.randn_like(x) * n_std
+        n_real = n[:, :, 0]
+        n_imag = n[:, :, 1]
+        n_complex = torch.complex(n_real, n_imag)
+        # Transmit Signals
+        y_complex = x_complex * h_complex + n_complex
+        # Employ the perfect CSI here
+        if detector == 'LS':
+            h_complex_conj = torch.conj(h_complex)
+            x_est_complex = y_complex * h_complex_conj / (h_complex * h_complex_conj)
+        elif detector == 'MMSE':
+            # MMSE Detector
+            h_complex_conj = torch.conj(h_complex)
+            a = h_complex * h_complex_conj + (n_std * n_std * 2)
+            x_est_complex = y_complex * h_complex_conj / a
+        else:
+            raise ValueError("detector must be 'LS' or 'MMSE'")
+        x_est_real = torch.real(x_est_complex)
+        x_est_img = torch.imag(x_est_complex)
+
+        x_est_real = x_est_real.unsqueeze(-1)
+        x_est_img = x_est_img.unsqueeze(-1)
+
+        x_est = torch.cat([x_est_real, x_est_img], dim=-1)
+        x_est = x_est.view(bs, sent_len, -1)
+
+        # method 1
+        noise_level = n_std * torch.ones(bs, sent_len, 1)
+        h_real = h_real * torch.ones(bs, sent_len, 1)
+        h_imag = h_imag * torch.ones(bs, sent_len, 1)
+        h = torch.cat((h_real, h_imag), dim=-1)
+        out1 = torch.cat((h, x_est), dim=-1)   # [bs, sent_len, 2 + d_model]
+
+        # method 2
+        y_complex_real = torch.real(y_complex)
+        y_complex_img = torch.imag(y_complex)
+        y_complex_real = y_complex_real.unsqueeze(-1)
+        y_complex_img = y_complex_img.unsqueeze(-1)
+        y = torch.cat([y_complex_real, y_complex_img], dim=-1)
+        y = y.view(bs, sent_len, -1)
+        out2 = torch.cat((h, y), dim=-1)  # [bs, sent_len, 2 + d_model]
+
+        return x_est
+
+
--- models/semantic.py
+++ models/semantic.py
@@ -1,10 +1,13 @@
-'''
+"""
 Author: LOTEAT
 Date: 2023-05-30 21:28:34
-'''
+"""
 import torch
 from torch import nn
 import numpy as np
+import math
+from .utils import LayerNorm
+
 
 def position_encoding(position, d_model):
     """
@@ -22,241 +25,258 @@ def position_encoding(position, d_model):
     pos_encoding = angle_set[None, ...]
     return torch.from_numpy(pos_encoding)
 
-class AttentionLayer(nn.Module):
-    def __init__(self, d_model, num_heads):
-        super(AttentionLayer, self).__init__()
+
+
+def attention(query, key, value, mask=None, dropout=None):
+    "Compute 'Scaled Dot Product Attention'"
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = scores.softmax(dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
+
+
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Embeddings, self).__init__()
+        self.lut = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
+
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, num_heads, d_model, dropout=0.1):
+        "Take in model size and number of heads."
+        super(MultiHeadedAttention, self).__init__()
+        assert d_model % num_heads == 0
+
         self.d_model = d_model
         self.num_heads = num_heads
         self.head_dim = d_model // num_heads
-    
-        assert d_model % self.num_heads == 0
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
 
-        # TODO
+        assert d_model % self.num_heads == 0
+        
         self.wq = nn.Linear(d_model, d_model, bias=False)
         self.wk = nn.Linear(d_model, d_model, bias=False)
         self.wv = nn.Linear(d_model, d_model, bias=False)
-        
+
         self.dense = nn.Linear(d_model, d_model)
         
-    def scale_dot_product_attention(self, q, k, v, mask):
-        """
-        softmax(Q*K^t/\sqrt(d_k))*V
-        where the dimension of Q is [, seq_len_q, depth]
-        the dimension of K is [, seq_len_k, depth]
-        the dimension of V is  [, seq_len_v, depth_v]
-        Q*K^t = [, seq_len_q, seq_len_k]
-    
-        Notice seq_len_k = seq_len_v
-    
-        mask uses in the Q*K^T ...[, seq_len_q, seq_len_k]
-    
-        the output is [, seq_len_q, depth_v]
-        """
-        attn_weights = torch.matmul(q, k.t()) / np.sqrt(self.d_model)
+    def forward(self, query, key, value, mask=None):
+        "Implements Figure 2"
         if mask is not None:
-            attn_weights += (mask * -1e9) 
-        attn_weights = nn.Softmax(dim=-1)(attn_weights)
-        outputs = torch.matmul(attn_weights, v)
-        return outputs, attn_weights
-    
-    def split_heads(self, x):
-        bs = x.shape[0]
-        length = x.shape[1]
-        depth = (self.d_model // self.num_heads)
-        x = x.view([bs, length, self.num_heads, depth])
-        return x.permute(0, 2, 1, 3)
-    
-    def combined_heads(self, x):
-        bs = x.shape[0]
-        length = x.shape[1]
-        # [batch, length, num_heads, depth]
-        x = x.permutate([0, 2, 1, 3]) 
-        return x.reshape([bs, length, self.d_model]) 
-    
-    def forward(self, v, k, mask):
+            # Same mask applied to all h heads.
+            mask = mask.unsqueeze(1)
+        nbatches = query.size(0)
+        
         q = self.wq(q)
-        k = self.wk(k)
-        v = self.wv(v)
-        
-        q = self.split_heads(q) # (batch_size, num_heads, seq_len_q, depth) where d_model = num_heads*depth
-        k = self.split_heads(k) # (batch_size, num_heads, seq_len_k, depth) where d_model = num_heads*depth
-        v = self.split_heads(v) # (batch_size, num_heads, seq_len_v, depth) where d_model = num_heads*depth
-        
-        scaled_attention, attention_weights = self.scale_dot_product_attention(q, k, v, mask)
-        #[batch_size, seq_len_q, depth_v]
-        attention_output = self.combined_heads(scaled_attention)
-                
-        multi_outputs = self.dense(attention_output)
-        return multi_outputs, attention_weights
-        
 
-class FeedForward(nn.Module):
+        # 1) Do all the linear projections in batch from d_model => h x d_k
+        query, key, value = [
+            lin(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+            for lin, x in zip((self.wq, self.wk, self.wv), (query, key, value))
+        ]
+
+        # 2) Apply attention on all the projected vectors in batch.
+        x, self.attn = attention(
+            query, key, value, mask=mask, dropout=self.dropout
+        )
+
+        # 3) "Concat" using a view and apply a final linear.
+        x = (
+            x.transpose(1, 2)
+            .contiguous()
+            .view(nbatches, -1, self.h * self.d_k)
+        )
+        return self.dense(x), self.attn
+
+class PositionwiseFeedForward(nn.Module):
+    "Implements FFN equation."
+
     def __init__(self, d_model, d_ff):
-        super(FeedForward, self).__init__()
-        self.d_model = d_model
-        self.dff = d_ff
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.ac_fun = nn.ReLU()
+        self.w_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        return self.w_2(self.ac_fun(self.w_1(x)))
+
+
+class SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.
+    """
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, attn_out):
+        "Apply residual connection to any sublayer with the same size."
+        return self.norm(x + self.dropout(attn_out))
     
-    def point_wise_feed_forward_network(d_model, dff):
-        '''
-        This is point_wise_feed_forward_network
-        FFN = max(0, x*W_1 + b_1)*W_2 +b_2
-        '''
-        return nn.ModuleList([
-            nn.Linear(d_model, dff),
-            nn.ReLU(),
-            nn.Linear(dff, d_model),
-        ])
-
-        
-        
-            
-
 class EncoderLayer(nn.Module):
-    '''
-    This is encoder layer, which includes two sublayers, multihead and feed forward.
-    '''
-    def __init__(self, d_model, num_heads, dff, drop_pro = 0.1):
+    "Encoder is made up of self-attn and feed forward (defined below)"
+
+    def __init__(self, size, d_model, num_heads, dff, dropout):
         super(EncoderLayer, self).__init__()
-        
-        self.attention_layer = AttentionLayer(d_model, num_heads)
-        self.feed_layer = FeedForward(d_model, dff)
-        
-        self.layernorm1 = nn.LayerNorm(eps=1e-6)
-        self.layernorm2 = nn.LayerNorm(epsilon=1e-6)
-        
-        self.dropout1 = nn.Dropout(drop_pro)
-        self.dropout2 = nn.Dropout(drop_pro)
-        
-    def forward(self, x, training, mask):
-        # attention: the layernorm(x + sublayer(x)) should be replaced by 
-        # x + sublayer(LayerNorm(x)) 
-        attn_output, _ = self.attention_layer(x, x, x, mask) #这个地方有点问题
-        attn_output = self.dropout1(attn_output, training = training)
-        output1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
-        
-        ffn_output = self.feed_layer(output1)
-        ffn_output = self.dropout2(ffn_output, training = training)
-        output2 = self.layernorm2(output1 + ffn_output) # (batch_size, input_seq_len, d_model)
-        
-        return output2
+        self.self_attn = MultiHeadedAttention(num_heads, d_model)
+        self.sublayer1 = SublayerConnection(size, dropout)
+        self.feed_forward = PositionwiseFeedForward(d_model, dff)
+        self.sublayer2 = SublayerConnection(size, dropout)
+        self.size = size
 
-
-
-
+    def forward(self, x, mask):
+        "Follow Figure 1 (left) for connections."
+        attn_output, _ = self.self_attn(x, x, x, mask)  
+        attn_output = self.sublayer1(x, attn_output)
+        ffn_output = self.feed_forward(attn_output)
+        ffn_output = self.sublayer2(attn_output, ffn_output)
+        return ffn_output
 
 class SemanticEncoder(nn.Module):
-    def __init__(self, num_layers, num_heads, d_model, dff, input_vocab_size, 
-             maximum_position_encoding=512, dropout_pro=0.1):
+    def __init__(
+        self,
+        num_layers,
+        num_heads,
+        d_model,
+        dff,
+        input_vocab_size,
+        maximum_position_encoding=512,
+        dropout_pro=0.1,
+    ):
         super(SemanticEncoder, self).__init__()
         self.d_model = d_model
         self.dff = dff
         self.num_layers = num_layers
         self.target_vocab_size = input_vocab_size
-        self.embedding = nn.Embedding(input_vocab_size, d_model) 
+        self.embedding = Embeddings(input_vocab_size, d_model)
         self.pos_encoding = position_encoding(maximum_position_encoding, self.d_model)
-        self.encoder = [EncoderLayer(d_model, num_heads, dff, dropout_pro) for _ in range(num_layers)]
-
         self.dropout = nn.Dropout(dropout_pro)
-    
+        self.encoder = [
+            EncoderLayer(d_model, d_model, num_heads, dff, dropout_pro)
+            for _ in range(num_layers)
+        ]
+
     def forward(self, x, training, mask):
         seq_len = x.shape[1]
-        
         # Embedding
-        x = self.embedding(x) 
-        x *= torch.sqrt(self.d_model)
+        x = self.embedding(x)
         # positional Encoding
         x += self.pos_encoding[:, :seq_len, :]
-        
         # Dropout
-        x = self.dropout(x, training = training)
-        
+        x = self.dropout(x, training=training)
         # Encoder
         for i in range(self.num_layers):
             x = self.encoder[i](x, training, mask)
-            
+
         return x
-    
-    
-    
+
+
 class DecoderLayer(nn.Module):
-    '''
-    This is decoder leayer, which includes three layers, 
-    1. multihead, 
-    2. masked multihead 
+    """
+    This is decoder leayer, which includes three layers,
+    1. multihead,
+    2. masked multihead
     3. feed forward
-    '''
-    def __init__(self, d_model, num_heads, dff, drop_pro = 0.1):
+    """
+
+    def __init__(self, d_model, num_heads, dff, drop_pro=0.1):
         super(DecoderLayer, self).__init__()
-        
-        self.attention_layer1 = AttentionLayer(d_model, num_heads) #masked
+
+        self.attention_layer1 = AttentionLayer(d_model, num_heads)  # masked
         self.attention_layer2 = AttentionLayer(d_model, num_heads)
-        
+
         self.ffn = FeedForward(d_model, dff)
-        
+
         self.layernorm1 = nn.LayerNorm(eps=1e-6)
         self.layernorm2 = nn.LayerNorm(eps=1e-6)
         self.layernorm3 = nn.LayerNorm(eps=1e-6)
-        
+
         self.dropout1 = nn.Dropout(drop_pro)
         self.dropout2 = nn.Dropout(drop_pro)
         self.dropout3 = nn.Dropout(drop_pro)
-        
+
     def forward(self, x, enc_output, training, look_ahead_mask, padding_mask):
         attn1, attn_weights1 = self.attention_layer1(x, x, x, look_ahead_mask)
-        attn1 = self.dropout1(attn1, training = training)
+        attn1 = self.dropout1(attn1, training=training)
         output1 = self.layernorm1(x + attn1)
-        
-        attn2, attn_weights2 = self.attention_layer2(enc_output, enc_output, output1, padding_mask)
-        attn2 = self.dropout2(attn2, training = training)
+
+        attn2, attn_weights2 = self.attention_layer2(
+            enc_output, enc_output, output1, padding_mask
+        )
+        attn2 = self.dropout2(attn2, training=training)
         output2 = self.layernorm2(attn2 + output1)
-        
+
         ffn_output = self.ffn(output2)
-        ffn_output = self.dropout3(ffn_output, training = training)
-        output3 = self.layernorm3(ffn_output + output2)  # (batch_size, target_seq_len, d_model)
-        
+        ffn_output = self.dropout3(ffn_output, training=training)
+        output3 = self.layernorm3(
+            ffn_output + output2
+        )  # (batch_size, target_seq_len, d_model)
+
         return output3, attn_weights1, attn_weights2
-        
+
 
 class SemanticDecoder(nn.Module):
-    '''
-    1. Output Embedding 
-    2. Positional Encoding 
+    """
+    1. Output Embedding
+    2. Positional Encoding
     3. N decoder layers
-    '''
-    def __init__(self, num_layers, d_model, num_heads, dff, target_vocab_size,
-               maximum_position_encoding=512, dropout_pro=0.1):
+    """
+
+    def __init__(
+        self,
+        num_layers,
+        d_model,
+        num_heads,
+        dff,
+        target_vocab_size,
+        maximum_position_encoding=512,
+        dropout_pro=0.1,
+    ):
         super(SemanticDecoder, self).__init__()
 
         self.d_model = d_model
         self.num_layers = num_layers
-    
+
         self.embedding = nn.Embedding(target_vocab_size, d_model)
         self.pos_encoding = position_encoding(maximum_position_encoding, d_model)
-    
-        self.dec_layers = [DecoderLayer(d_model, num_heads, dff, dropout_pro)
-                       for _ in range(num_layers)]
+
+        self.dec_layers = [
+            DecoderLayer(d_model, num_heads, dff, dropout_pro)
+            for _ in range(num_layers)
+        ]
         self.dropout = nn.Dropout(dropout_pro)
         # prediction layer
         self.final_layer = nn.Linear(target_vocab_size, target_vocab_size)
-    
-    def forward(self, x, enc_output, training, look_ahead_mask, padding_mask):
 
+    def forward(self, x, enc_output, training, look_ahead_mask, padding_mask):
         seq_len = x.shape[1]
         attention_weights = {}
-    
+
         x = self.embedding(x)  # (batch_size, target_seq_len, d_model)
         x *= torch.sqrt(self.d_model)
         x += self.pos_encoding[:, :seq_len, :]
-    
+
         x = self.dropout(x, training=training)
 
         for i in range(self.num_layers):
-            x, block1, block2 = self.dec_layers[i](x, enc_output, training,
-                                             look_ahead_mask, padding_mask)
-      
-        attention_weights['decoder_layer{}_block1'.format(i+1)] = block1
-        attention_weights['decoder_layer{}_block2'.format(i+1)] = block2
-        
+            x, block1, block2 = self.dec_layers[i](
+                x, enc_output, training, look_ahead_mask, padding_mask
+            )
+
+        attention_weights["decoder_layer{}_block1".format(i + 1)] = block1
+        attention_weights["decoder_layer{}_block2".format(i + 1)] = block2
+
         x = self.final_layer(x)
 
         # x.shape == (batch_size, target_seq_len, d_model)
